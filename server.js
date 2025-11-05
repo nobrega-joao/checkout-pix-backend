2095aa34e4eb29a7d5af1ca5d20c6534b3a591f7
João Victor <83452340+nobrega-joao@users.noreply.github.com>
2025-11-05 03:41:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -131,5 +131,8 @@ app.get("/check-payment-local/:transactionId", (req, res) => {
 
 
 
-app.listen(3000, () => console.log("✅ Servidor rodando em http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("✅ Servidor rodando na porta " + PORT));
+
+
 
